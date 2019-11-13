6d0e3a70d3a1949935f10f8c595477f0e9160c0f
João Victor <victor_campelo@outlook.com>
2019-11-13 00:18:17 -0300

    refactor: steps to compiler, install and execute device driver

--- ordDriver/commands.sh
+++ ordDriver/commands.sh
@@ -1,34 +1,73 @@
 #! /bin/bash
 
+mode="0666"
+
+sudo rm /dev/sortlist
+
+sudo make clean
+
+echo "COMMAND LSPCI: "
+
 lspci | grep $1
 
-echo "------------------------"
 
-dmesg | grep $1 > file.txt 
-tail -n 10 file.txt
+echo "COMMAND DMESG: "
 
-echo "------------------------"
+dmesg | tail -1
 
-/sbin/lsmod
-
-#If a driver is recognized by those commands 
-#but not by lscpi or dmesg, it means the driver 
-#is on the disk but not in the kernel. 
-#In this case, load the module with 
-#the modprobe command:
-#$1 = module name
-sudo modprobe $1
-
-sudo make
 
 mod=$1
 mod+=".ko"
 
+sudo rmmod $mod
+
+sudo make
+
 sudo insmod $mod
 
-echo "------------------------"
 
-dmesg | grep $1 > file.txt 
-tail -n 10 file.txt
+echo "COMMAND DMESG: "
 
-echo "------------------------"
+dmesg | tail -1
+
+
+echo "COMMAND LSMOD: "
+
+lsmod | head -1
+
+#major=$(cat /proc/devices | awk '{ if($2 == "memory"){ print $1 } }')
+major=`cat /proc/devices | grep $1 | awk '{print $1}'`
+
+echo "VALUE OF MAJOR NUMBER: $major"
+
+cat /proc/devices | grep $major
+
+echo "SHOW THE LIST OF /DEV: "
+ls -l /dev | grep $major | grep $1
+
+#echo "[CREATE]: mknod /dev/$1 --mode=$modo c $major 0  \n"
+#mknod /dev/$1 --mode=$mode c $major 0
+
+# echo -n 3>/dev/sortlist
+
+# dmesg | tail -1
+
+# echo -n 1>/dev/sortlist
+
+# dmesg | tail -1
+
+# echo -n 5>/dev/sortlist
+
+# dmesg | tail -1
+
+# echo -n 7>/dev/sortlist
+
+# dmesg | tail -1
+
+# echo -n 4>/dev/sortlist
+
+# dmesg | tail -1
+
+# cat /dev/sortlist
+
+# dmesg | tail -10
